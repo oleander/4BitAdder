713cf457bcb7cfa359e03e0bb948faf5c806cb33
Linus Oleander <linus@oleander.nu>
2011-01-19 17:17:08 +0100

    Implementing some non working code

--- lib/main.vhdl
+++ lib/main.vhdl
@@ -3,15 +3,27 @@ library ieee ;
   use ieee.numeric_std.all ;
   use ieee.std_logic_unsigned.all;
 
-entity adder is
+entity main is
   port (
-    X,Y : IN std_logic_vector(3 downto 0);
-    RESULT, C : OUT std_logic_vector(3 downto 0)
+    a,b : IN std_logic_vector(3 downto 0);
+    cin : IN std_logic;
+    s   : OUT std_logic_vector(3 downto 0);
+    cut : OUT std_logic
   ) ;
 end entity ; -- adder
 
-architecture arch of adder is
-
+architecture arch of main is
+  signal sum : std_logic_vector(4 downto 0);
+  signal adjust: std_logic;
+  signal save: std_logic_vector(3 downto 0);
 begin
-
+  sum <= ('0' & a) + ('0' & b) + cin;
+  adjust <= '1' when sum > 9 else '0';
+  
+  -- Writing to outgoing port
+  sum <= sum when (adjust = '0') else sum + 6;
+  s <= sum(3 downto 0);
+  
+  -- Writing the carry to outgoing port
+  cut <= adjust;
 end architecture ; -- arch
